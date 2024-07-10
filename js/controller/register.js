da15bb69a4212746d11e01275860aa03724bf842
Ratusukmakomala <ratukomala28@gmail.com>
2024-07-10 13:38:44 +0700

    edit XMLHttpRequest

--- js/controller/register.js
+++ js/controller/register.js
@@ -11,7 +11,6 @@ document.getElementById('submit-btn').addEventListener('click', function() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
 
- // Membuat instance baru dari XMLHttpRequest
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://47.236.157.2:443/signup', true);
     xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
