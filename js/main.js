05812de168f9213aa8cc0c749c7888757a4677ab
isnaenti <isnaenti16@gmail.com>
2024-07-29 20:01:55 +0700

    add click listener and cookie

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-import { get } from "https://jscroot.github.io/api/croot.js";
-import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
-import {onClick} from 'https://jscroot.github.io/element/croot.js';
-import { custom_menuBtn, navbarSupportedContent } from '/js/src/custom.js';
+import { fetchData } from "https://jscroot.github.io/api/croot.js";
+import { retrieveCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { addClickListener } from 'https://jscroot.github.io/element/croot.js';
+import { initializeMenuButton, setupNavbar } from '/js/src/custom.js';
 
-// let cookie = getCookie("Login")
-// if (cookie == ""){
-//     alert("Anda Belum Sign In Boss");
-//     window.location.href = "https://anakilang-ai.github.io/login.html"
+// let userCookie = retrieveCookie("Login")
+// if (userCookie === ""){
+//     alert("Please sign in first");
+//     window.location.href = "https://anakilang-ai.github.io/login.html";
 // }
 
 custom_menuBtn();
