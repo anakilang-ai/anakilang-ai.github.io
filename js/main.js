a0a4da2373b864c2dab47a271ac698bc2b416e5d
isnaenti <isnaenti16@gmail.com>
2024-07-31 22:14:38 +0700

    insertmarker btn

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-import { fetchData } from "https://jscroot.github.io/api/croot.js";
-import { retrieveCookie } from "https://jscroot.github.io/cookie/croot.js";
-import { handleClick } from 'https://jscroot.github.io/element/croot.js';
-import { customMenuButton, navbarContent } from '/js/src/custom.js';
+import { get } from "https://jscroot.github.io/api/croot.js";
+import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
+import {onClick} from 'https://jscroot.github.io/element/croot.js';
+import { custom_menuBtn, navbarSupportedContent } from '/js/src/custom.js';
 
-// let userSession = retrieveCookie("Login")
-// if (!userSession){
-//     alert("Please log in to continue");
+// let cookie = getCookie("Login")
+// if (cookie == ""){
+//     alert("Anda Belum Sign In Boss");
 //     window.location.href = "https://anakilang-ai.github.io/login.html"
 // }
 
-customMenuButton();
-navbarContent();
+custom_menuBtn();
+navbarSupportedContent();
 
-handleClick('popup-closer', onPopupClose);
-handleClick('insertmarkerbutton', onMarkerSubmit);
-handleClick('hapusbutton', onMarkerDelete);
-handleClick('hitungcogbutton', retrieveAllCoordinates);
+onClick('popup-closer',onClosePopupClick);
+onClick('insertmarkerbutton',onSubmitMarkerClick);
+onClick('hapusbutton',onDeleteMarkerClick);
+onClick('hitungcogbutton',getAllCoordinates);
