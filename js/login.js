49d76be91640670d96b4758c04db8acebf0077b1
jxxzy <marjuniatiputri@gmail.com>
2024-08-02 15:37:18 +0700

    Changes the password field type

--- js/login.js
+++ js/login.js
@@ -1,17 +1,22 @@
+// Import the handleLogin function from the controller module
 import { handleLogin } from '../js/src/controller.js';
 
+// Wait for the DOM content to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
+  // Select the login form
   const form = document.getElementById("login-form");
 
+  // Handle form submission
   form.addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent default form submission
-    handleLogin(); // Handle login logic
+    handleLogin(); // Call the function to handle login
   });
 
   // Toggle password visibility
   const showPasswordCheckbox = document.getElementById("show-password");
   showPasswordCheckbox.addEventListener("change", function () {
     const passwordField = document.getElementById("password");
+    // Change the type of the password field based on the checkbox status
     passwordField.type = this.checked ? "text" : "password";
   });
 });
